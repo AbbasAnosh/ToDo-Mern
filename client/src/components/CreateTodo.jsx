c32b0e965f1e46105abf4ce4a98b9fa1e08be955
Abbas Anosh <abbasanosh123@gmail.com>
2024-04-12 12:42:11 -0400

    changes

--- client/src/components/CreateTodo.jsx
+++ client/src/components/CreateTodo.jsx
@@ -8,20 +8,21 @@ import {
   Stack,
   InputGroup,
   InputLeftAddon,
+  InputLeftElement,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { motion } from "framer-motion";
 import DatePicker from "react-datepicker";
-import { CalendarIcon } from "@chakra-ui/icons";
+import { CalendarIcon, TimeIcon } from "@chakra-ui/icons";
 
 import "react-datepicker/dist/react-datepicker.css";
-// import "./DatePickerStyles.css";
 
 const CreateTodo = ({ onAddTodo }) => {
   const [todo, setTodo] = useState({
     name: "",
     description: "",
     date: new Date(),
+    time: "",
     completed: false,
   });
   const toast = useToast();
@@ -34,6 +35,14 @@ const CreateTodo = ({ onAddTodo }) => {
     });
   };
 
+  const handleTimeChange = (e) => {
+    let value = e.target.value;
+    value = value.replace(/(\d{2})(?=\d)/g, "$1:").substring(0, 8);
+    setTodo({
+      ...todo,
+      time: value,
+    });
+  };
   const handleDateChange = (date) => {
     setTodo({
       ...todo,
@@ -42,11 +51,19 @@ const CreateTodo = ({ onAddTodo }) => {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const timeParts = todo.time.split(":");
+    const hours = parseInt(timeParts[0], 10);
+    const minutes = parseInt(timeParts[1], 10);
+    const seconds = timeParts.length > 2 ? parseInt(timeParts[2], 10) : 0;
+
+    const dateTime = new Date(todo.date.getTime());
+    dateTime.setHours(hours, minutes, seconds);
+
     const newData = {
       name: todo.name,
       description: todo.description,
       completed: todo.completed,
-      date: todo.date,
+      date: dateTime,
     };
 
     try {
@@ -55,7 +72,13 @@ const CreateTodo = ({ onAddTodo }) => {
         newData
       );
       onAddTodo(true);
-      setTodo({ name: "", description: "", completed: false });
+      setTodo({
+        name: "",
+        description: "",
+        date: new Date(),
+        time: "",
+        completed: false,
+      });
       toast({
         title: "Todo added.",
         description: "Your todo has been successfully added.",
@@ -94,6 +117,7 @@ const CreateTodo = ({ onAddTodo }) => {
         >
           <Stack>
             <Input
+              id="todoinput"
               placeholder="What is the name of the todo?"
               isRequired
               borderColor={"border"}
@@ -112,6 +136,7 @@ const CreateTodo = ({ onAddTodo }) => {
             />
 
             <Textarea
+              id="todotextarea"
               isRequired
               borderColor={"border"}
               borderWidth="1px"
@@ -123,22 +148,51 @@ const CreateTodo = ({ onAddTodo }) => {
               minWidth={{ base: "100%", md: "660px" }}
               backgroundColor="background"
             />
-            <InputGroup>
-              <InputLeftAddon
-                pointerEvents="none"
-                children={<CalendarIcon color="gray.300" />}
-                backgroundColor="pagebg"
-              />
-              <DatePicker
-                selected={todo.date}
-                onChange={handleDateChange}
-                showTimeSelect
-                timeIntervals={60}
-                dateFormat="MMMM d, yyyy h:mm aa"
-                customInput={<Input backgroundColor="background" />}
-                withPortal
-              />
-            </InputGroup>
+            <Stack
+              direction={{ base: "column", md: "row" }}
+              align="left"
+              spacing="2"
+            >
+              <InputGroup className="date-picker-container">
+                <InputLeftAddon
+                  children={<CalendarIcon color="gray.300" />}
+                  backgroundColor={"background"}
+                />
+                <DatePicker
+                  id="tododate"
+                  selected={todo.date}
+                  onChange={handleDateChange}
+                  customInput={<Input backgroundColor={"background"} />}
+                  dateFormat="MMMM d, yyyy"
+                  isClearable
+                  withPortal
+                  name="date"
+                  value={todo.date}
+                />
+              </InputGroup>
+
+              <InputGroup
+                backgroundColor={"background"}
+                borderRadius={"lg"}
+                maxWidth={"max-content"}
+              >
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={<TimeIcon color="gray.300" />}
+                />
+                <Input
+                  id="todotime"
+                  placeholder="HH:MM:SS"
+                  name="time"
+                  value={todo.time}
+                  onChange={handleTimeChange}
+                  pattern="\d{2}:\d{2}:\d{2}"
+                  maxLength="8"
+                  focusBorderColor="blue.500"
+                  errorBorderColor="red.300"
+                />
+              </InputGroup>
+            </Stack>
           </Stack>
         </motion.div>
         <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
